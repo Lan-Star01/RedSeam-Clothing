1fa07b3073b5246787bee1a97dee2d1d30543263
Lana Mezvrishvili <lana.mezvrishvili.08@gmail.com>
2025-09-20 20:04:23 +0400

    feat(auth): add login page with reactive form and AuthService integration

--- src/app/features/auth/pages/login-page/login-page.component.ts
+++ src/app/features/auth/pages/login-page/login-page.component.ts
@@ -1,11 +1,68 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../../../core/services/auth.service';
+import { Router, RouterModule } from '@angular/router';
+import { OperatorFunction } from 'rxjs';
+import { User } from '../../../../core/models/user.model';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
+  loginForm: any;
+  loading = false;
+  error: string | null = null;
+  showPassword = false;
 
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [
+        Validators.required, 
+        Validators.email,
+        Validators.minLength(3),
+        Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
+      ]],
+      password: ['', [Validators.required, Validators.minLength(3)]],
+    });
+  }
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) return;
+
+    this.loading = true;
+    this.error = null;
+
+    const { email, password } = this.loginForm.value;
+
+    this.authService
+      .login(email!, password!)
+      .pipe(
+        tap({
+          next: (user) => {
+            localStorage.setItem('token', user.token);
+            localStorage.setItem('user', JSON.stringify(user.user));
+            this.router.navigateByUrl('/products');
+          },
+          error: () => {
+            this.error = 'Invalid email or password';
+            this.loading = false;
+          },
+        })
+      )
+      .subscribe();
+  }
 }
